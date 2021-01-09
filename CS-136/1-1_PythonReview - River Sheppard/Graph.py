b5228b43b15913f4e00b31d0d071b7f47e48b6d6
River Sheppard <compsci.river@gmail.com>
2021-01-08 20:21:01 -0700

    Commented Graph.py
    added basic comments to Graph.py as well as some basic adjustments

--- CS-136/1-1_PythonReview - River Sheppard/Graph.py
+++ CS-136/1-1_PythonReview - River Sheppard/Graph.py
@@ -1,16 +1,21 @@
 #
 #River Sheppard
 #CSI 136
-#Description:
+#Description: Uses StdDraw to creat a graph that represents the data found by the Benford
+#script, the y-values on the side of the graph scale with the data and the precise value
+# of each bucket is displayed at the top of each column
+#
 
 import sys
 import StdDraw
 import Benford
 
 class Graph:
+    #Constructor
     def __init__(self):
         pass
 
+    #Counts the number of digits in an int
     def intLength(self, num):
         count = 1
         while int(num) >= 1:
@@ -19,6 +24,7 @@ class Graph:
                 count += 1
         return count
 
+    #Finds the largest number in a list
     def greatestInt(self, intList):
         greatInt = 0
         for int in intList:
@@ -26,6 +32,7 @@ class Graph:
                 greatInt = int
         return greatInt
 
+    #Finds the number with which so scal the y-axis
     def graphHeight(self, num):
         intLen = self.intLength(num)
         height = 5 * pow(10,intLen-1)
@@ -33,7 +40,8 @@ class Graph:
             height = pow(10,intLen)
         return height
 
-    def graphDraw(self, height):
+    #Draws the blank graph before any data in used
+    def graphDraw(self):
         StdDraw.setCanvasSize(600,600)
         StdDraw.setXscale(0,600)
         StdDraw.setYscale(0,600)
@@ -45,18 +53,7 @@ class Graph:
         StdDraw.line(45,550,55,550)
         StdDraw.line(48,425,52,425)
         StdDraw.line(45,300,55,300)
-        StdDraw.line(48,175,52,175)
-
-        if height >= 100:
-            StdDraw.text(25,550, str(height))
-            StdDraw.text(25,425, str(int(3*height/4)))
-            StdDraw.text(25,300, str(int(height/2)))
-            StdDraw.text(25,175, str(int(height/4)))
-        else:
-            StdDraw.text(25,550, str(height))
-            StdDraw.text(25,425, str(3*height/4))
-            StdDraw.text(25,300, str(height/2))
-            StdDraw.text(25,175, str(height/4))  
+        StdDraw.line(48,175,52,175) 
 
         StdDraw.line(75,48,75,52)
         StdDraw.text(75,25, "0")
@@ -81,9 +78,22 @@ class Graph:
         StdDraw.line(550,45,550,55)
         StdDraw.show(0.0)
 
+    #Calls the graph and then adds the data points
     def graphing(self, intList):
+        self.graphDraw()
+
         height = self.graphHeight(self.greatestInt(intList))
-        self.graphDraw(height)
+
+        if height >= 100:
+            StdDraw.text(25,550, str(height))
+            StdDraw.text(25,425, str(int(3*height/4)))
+            StdDraw.text(25,300, str(int(height/2)))
+            StdDraw.text(25,175, str(int(height/4)))
+        else:
+            StdDraw.text(25,550, str(height))
+            StdDraw.text(25,425, str(3*height/4))
+            StdDraw.text(25,300, str(height/2))
+            StdDraw.text(25,175, str(height/4)) 
         
         for i in range(0,len(intList)):
             StdDraw.setPenColor(StdDraw.BLUE)
@@ -102,7 +112,6 @@ if __name__ == "__main__":
     graph = Graph()
     ben = Benford.Benford()
     nums = ben.nthDigitTally(int(sys.argv[1]), ben.readMysteriousNumbers(sys.argv[2]))
-    print(nums)
     graph.graphing(nums)
 
 
